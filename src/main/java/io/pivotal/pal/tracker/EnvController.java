5c0c9ca0b15199bb1f107b570d936bf754053c19
pal_user <cthom409@ford.com>
2018-07-30 17:01:14 -0400

    Finished Deploying app to Cloud Foundry Lab

---
+++ src/main/java/io/pivotal/pal/tracker/EnvController.java
@@ -0,0 +1,38 @@
+package io.pivotal.pal.tracker;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+public class EnvController {
+
+    private final String port;
+    private final String memoryLimit;
+    private final String cfInstanceIndex;
+    private final String cfInstanceAddr;
+
+    public EnvController(@Value("${PORT:NOT SET}") String port,
+                         @Value("${MEMORY_LIMIT:NOT SET}") String memoryLimit,
+                         @Value("${CF_INSTANCE_INDEX:NOT SET}") String cfInstanceIndex,
+                         @Value("${CF_INSTANCE_ADDR:NOT SET}") String cfInstanceAddr) {
+
+        this.port = port;
+        this.memoryLimit = memoryLimit;
+        this.cfInstanceIndex = cfInstanceIndex;
+        this.cfInstanceAddr = cfInstanceAddr;
+    }
+
+    @GetMapping("/env")
+    public HashMap<String, String> getEnv() {
+        HashMap<String, String> env = new HashMap<>();
+        env.put("PORT", port);
+        env.put("MEMORY_LIMIT", memoryLimit);
+        env.put("CF_INSTANCE_INDEX", cfInstanceIndex);
+        env.put("CF_INSTANCE_ADDR", cfInstanceAddr);
+        return env;
+    }
+}
